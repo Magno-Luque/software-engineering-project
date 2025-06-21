a718c4ab634b694800c8625d5938f37619547d29
Richard-ML <140691274+Magno-Luque@users.noreply.github.com>
2025-06-21 13:12:58 -0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,10 +7,5 @@ class Config:
     Configuración principal de la aplicación Flask
     """
     
-    # Configuración de la base de datos
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://app_db:12344321@yamabiko.proxy.rlwy.net:48389/db_proof'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # Clave secreta para sesiones
-    SECRET_KEY = 'clave_secreta_segura'
+
     
